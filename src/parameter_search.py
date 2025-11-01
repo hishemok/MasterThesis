0735a3d58b510d6224b2c6a771d8ee0573e8f832
Hishem Kløvnes <hishemok@uio.no>
2025-11-01 19:32:46 +0100

    new loss function - only optimize

--- src/parameter_search.py
+++ src/parameter_search.py
@@ -75,6 +75,26 @@ def calculate_parities(evals, evecs, P):
 
     return E_even, E_odd
 
+def Majorana_purity_metric(n, eigenvecs, create_t, ann_t):
+    """Used to calculate the Majorana purity metric for a set of eigenvectors
+        M_n = ( ⟨c_n†⟩ - ⟨c_n⟩  ) / ( ⟨c_n†c_n⟩ + ⟨c_nc_n†⟩ )
+        M -> 0 for pure Majorana state localized on site n
+        M -> 1 for delocalized fermionic state
+        M = ∑_n M_n
+    ."""
+    M = torch.zeros(n, dtype=torch.complex128, device=eigenvecs.device)
+    for i in range(n):
+        cd_t = create_t[i]
+        c_t = ann_t[i]
+        exp_cd = torch.sum(torch.conj(eigenvecs) * (cd_t @ eigenvecs), dim=0)
+        exp_c  = torch.sum(torch.conj(eigenvecs) * (c_t @ eigenvecs), dim=0)
+        exp_n  = torch.sum(torch.conj(eigenvecs) * (cd_t @ c_t @ eigenvecs), dim=0)
+        exp_nbar = torch.sum(torch.conj(eigenvecs) * (c_t @ cd_t @ eigenvecs), dim=0)
+
+        M[i] = torch.mean(torch.abs((exp_cd - exp_c) / (exp_n + exp_nbar + 1e-12)))
+
+    M_total = torch.sum(torch.abs(M))
+    return M_total.real
 
 def degeneracy_and_gap_loss(H, P, n, weight_vec=None, gap_target=0.5, gap_weights=None):
     # H: Hermitian torch matrix
@@ -112,6 +132,62 @@ def degeneracy_and_gap_loss(H, P, n, weight_vec=None, gap_target=0.5, gap_weight
 
     return 10 * Ldeg + 0.1 * gap_pen
 
+
+
+
+def degeneracy_and_gap_loss2(H, P, n, weight_vec=None, gap_target=0.5, gap_weights=None, theta=None):
+    # H: Hermitian torch matrix
+    H = 0.5 * (H + H.conj().T) ## Just because the optimizer can introduce small non-Hermiticities
+
+    evals, evecs = torch.linalg.eigh(H)  # evals sorted ascending
+
+    even_states, odd_states = calculate_parities(evals, evecs, P)
+
+    # Just in case
+    E_even, _ = torch.sort(even_states)
+    E_odd,  _ = torch.sort(odd_states)
+
+    m = min(len(E_even), len(E_odd)) # Minimum number of elements
+    if m == 0:
+        raise  ValueError("No states in one of the parity sectors \n Proceed to next restart")  # bad configuration Penalty
+
+    if len(E_even) != len(E_odd):
+        # print("Warning: unequal number of even and odd states:", len(E_even), len(E_odd))
+        raise ValueError("Unequal number of even and odd states \n Bad configuration, proceed to next restart")
+
+
+    # # default weights
+    if weight_vec is None:
+        weight_array = np.linspace(1, 0.1, 2*m-1)**2# First half for Degeneracy, second half for gaps
+        weight_vec = torch.tensor(weight_array, device=H.device)
+
+    penalty_array = torch.zeros(2*m-1, device=H.device) # First half for Degeneracy, second half for gaps
+
+    deg_terms = torch.abs(E_even - E_odd) # Cut out unequal lengths
+    w = weight_vec.to(H.device)
+    degeneracy_terms = w[:m] * deg_terms
+    penalty_array[:m] = degeneracy_terms
+
+
+    even_gaps = E_even[1:] - E_even[:-1]
+    odd_gaps = E_odd[1:] - E_odd[:-1]
+    worst_gaps = torch.min(torch.stack([even_gaps, odd_gaps]), dim=0).values
+
+    # If gap < 0.5 penalize, else 0
+    gap_penalties = torch.nn.functional.softplus(gap_target - worst_gaps)
+    gap_terms = w[m:] * gap_penalties
+    penalty_array[m:] = gap_terms
+
+    cre_t, ann_t, _ = precompute_ops(n)
+    cre_t = [to_torch(m, device=H.device) for m in cre_t]
+    ann_t = [to_torch(m, device=H.device) for m in ann_t]   
+    # Calculate Majorana purity metric for the eigenvectors
+    Majorana_metric = Majorana_purity_metric(n, evecs, cre_t, ann_t)
+    # Optimal is 0, so we can add it directly to the penalty
+    total_penalty = torch.sum(penalty_array) + Majorana_metric
+    return total_penalty
+
+
 def print_params(theta, n):
     t = theta[0]
     U = theta[1:1+(n-1)]
@@ -121,6 +197,8 @@ def print_params(theta, n):
     print(f"U = {U}")
     print(f"ε = {eps}")
     print(f"Δ = {Delta:.4f}")
+
+
 
 
 def optimize_params(n, device='cpu', restarts=8, iters=600):
@@ -195,11 +273,11 @@ def optimize_params2(n, device='cpu', restarts=8, iters=600):
         # initialize torch parameter vector (t, U0..U_{n-2}, eps0..eps_{n-1}, Delta)
         # choose sensible ranges: t ~ [0.2,2], U~[0.2,2], eps ~ [-1,1], Delta ~ [0,2]
         rng = np.random.RandomState(seed=r)
-        t0 = 0.5 + rng.rand()*1.5
+        t0 = 0.5 + rng.rand()*.5
         # U0 = 0.5 + abs(rng.rand(n-1)*1.5)
         # eps0 = -0.5 + rng.rand(n)*1.0
         # D0 = 0.5 + rng.rand()*1.5
-        U0 =  0.5 + abs(rng.rand()*1.5)
+        U0 =  0.5 + abs(rng.rand()*5)
         U = [U0 for _ in range(n-1)]
 
         rnd1 = rng.rand()
@@ -217,26 +295,26 @@ def optimize_params2(n, device='cpu', restarts=8, iters=600):
         theta = torch.tensor(theta0, dtype=torch.float64, device=device, requires_grad=True)
 
         optimizer = torch.optim.Adam([theta], lr=0.05)
+        # optimizer = torch.optim.LBFGS([theta], lr=0.5, max_iter=100)
+        # def closure():
+        #     optimizer.zero_grad()
+        #     with torch.no_grad():
+        #         theta.data.clamp_(-10, 10)
+        #     H = build_H_torch(n, theta, cre_t, ann_t, num_t)
+        #     P = parity_operator_torch(n)
+        #     loss = degeneracy_and_gap_loss2(H, P, n, theta=theta)
+        #     loss.backward()
+        #     return loss
+
+        # # Outer loop for restarts / convergence monitoring
+        # for it in range(iters):
+        #     loss = optimizer.step(closure)  # LBFGS needs the closure here
+    
         for it in range(iters):
             optimizer.zero_grad()
             H = build_H_torch(n, theta, cre_t, ann_t, num_t)  # accepts real theta
             P = parity_operator_torch(n)
-            loss = degeneracy_and_gap_loss(H, P, n)
-
-            # # --- Enforce known sweet-spot relations ---
-            # t = theta[0]
-            # U = theta[1:1+(n-1)]
-            # eps = theta[1+(n-1):1+(n-1)+n]
-            # Delta = theta[-1]
-
-            # eps_penalty = torch.mean((eps[0] + U[0]/2)**2 + (eps[-1] + U[-1]/2)**2)
-            # if n > 2:
-            #     eps_penalty += torch.mean((eps[1:-1] + U[:-1])**2)
-            # Delta_penalty = torch.mean((Delta - (t + U[0]/2))**2)
-
-            # # Scale the penalty term
-            # loss += 0.1 *  (eps_penalty + Delta_penalty)
-            # # ------------------------------------------
+            loss = degeneracy_and_gap_loss2(H, P, n)
 
             loss.backward()
             optimizer.step()
@@ -279,6 +357,14 @@ def plot_parity_spectrum(n, theta):
         print(f"Eigenvalue {E:.4f} belongs to {sector} parity sector, with parity {p:.4f}")
     print("")
 
+    cre_t, ann_t, _ = precompute_ops(n)
+    cre_t = [to_torch(m, device=H.device) for m in cre_t]
+    ann_t = [to_torch(m, device=H.device) for m in ann_t]   
+    # Calculate Majorana purity metric for the eigenvectors
+    Majorana_metric = Majorana_purity_metric(n, to_torch(evecs) , cre_t, ann_t)
+    print(f"Majorana purity metric: {Majorana_metric:.6f}\n")
+
+
     evens = [(E, v) for E, v, p in zip(evals, evecs.T, parities) if p >= 0]
     odds  = [(E, v) for E, v, p in zip(evals, evecs.T, parities) if p <  0]
     y_even = [E for E,_ in evens]
@@ -290,14 +376,7 @@ def plot_parity_spectrum(n, theta):
         if abs(Ee - Eo) < 1e-3:
             degeneracy_lines.append(Ee)
 
-    # plt.figure(figsize=(5,4))
-    # plt.hlines(y_even, -0.2, 0.2, color='tab:blue')
-    # plt.hlines(y_odd,  0.8, 1.2, color='tab:red')
-    # plt.hlines(degeneracy_lines, 0.2, 0.8, color='tab:gray', linestyles='dashed', label='Degeneracies')
-    # plt.xticks([0,1], ['Even','Odd'])
-    # plt.title(f"Parity-resolved energy spectrum for {n}-dot system")
-    # plt.ylabel("Energy")
-    # plt.show()
+
     t = theta[0]
     U = theta[1:1+(n-1)]
     eps = theta[1+(n-1):1+(n-1)+n]
@@ -354,10 +433,10 @@ def plot_parity_spectrum(n, theta):
 
 
 if __name__ == "__main__":
-    n = 2
-    best_theta, best_loss = optimize_params(n=n, device='cpu', restarts=5, iters=600)
-    plot_parity_spectrum(n, best_theta)
-    for n in range(3,5):
+    # n = 2
+    # best_theta, best_loss = optimize_params(n=n, device='cpu', restarts=5, iters=600)
+    # plot_parity_spectrum(n, best_theta)
+    for n in range(2,5):
         print("Optimizing for n =", n)
         best_theta, best_loss = optimize_params2(n=n, device='cpu', restarts=5, iters=600)
         plot_parity_spectrum(n, best_theta)
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,3 +1,4 @@
-    # print("Even states:", even_states)
-    # E_even = torch.tensor([E for E,_ in even_states], device=H.device, dtype=torch.float64)
-    # E_odd  = torch.tensor([E for E,_ in odd_states], device=H.device, dtype=torch.float64)
+  # print("gap_penalties:", gap_penalties)
+    # print("w:", w)
+    # print("w[:m]:", w[:m])
+    # print("w[m:]:", w[m:])
